787925cdb97cf3537710838af5502249205b1e90
myalilprincy-ux <myalilprincy@gmail.com>
2026-05-27 11:35:44 +0530

    Update port_scanner.py

--- Task-3-Penetration-Testing-Toolkit/port_scanner.py
+++ Task-3-Penetration-Testing-Toolkit/port_scanner.py
@@ -1,14 +1,29 @@
 import socket
 
-target = input("Enter IP Address: ")
+# Scan ports
 
-for port in range(1, 100):
+def scan_port(target, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((target, port))
 
-    result = sock.connect_ex((target, port))
+        if result == 0:
+            print(f"[OPEN] Port {port}")
 
-    if result == 0:
-        print(f"Port {port} is open")
+        sock.close()
 
-    sock.close()
+    except Exception as e:
+        print(f"Error: {e}")
+
+# Main
+
+def main():
+    target = input("Enter target IP: ")
+
+    for port in range(1, 1025):
+        scan_port(target, port)
+
+if __name__ == "__main__":
+    main()
